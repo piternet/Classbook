241eef5c9c7a2b979b291306f71e555495f7b79e
Piotr Nosek <nosekpr@gmail.com>
2017-12-11 10:32:50 -0800

    Utworzylem model Tagu

--- main/models.py
+++ main/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+	name = models.CharField(max_length=80)
+
+	def __str__(self):
+		return self.name
 
 class Post(models.Model):
 	title = models.CharField(max_length=200)
 	content = models.CharField(max_length=10000)
 	publish_date = models.DateField()
 	user = models.ForeignKey(User)
+	tags = models.ManyToManyField(Tag)
 
 	def __str__(self):
 		return "Tytul: " + self.title + ", Tresc: " + self.content
