f32f8d33d2e04110ac4366d85473b216c9bf68d1
SilentStormm <kazek.cias@gmail.com>
2017-12-16 15:21:06 +0100

    Uwtorzony model Comment, wyświetlanie tagów i komentarzy pod postami

--- classbook/wsgi.py
+++ classbook/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for classbook project.
+WSGI config for Classbook project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
--- main/models.py
+++ main/models.py
@@ -7,12 +7,19 @@ class Tag(models.Model):
 	def __str__(self):
 		return self.name
 
+class Comment(models.Model):
+	user = models.ForeignKey(User)
+	date = models.DateField() # DateTimeField(auto_now_add=True)
+	content = models.CharField(max_length=300)
+	
 class Post(models.Model):
-	title = models.CharField(max_length=200)
+	title = models.CharField(max_length=150)
 	content = models.CharField(max_length=10000)
-	publish_date = models.DateField()
+	publish_date = models.DateField() # DateTimeField(auto_now_add=True)
+#	edit_date = models.DateTimeField(auto_now=True)
 	user = models.ForeignKey(User)
 	tags = models.ManyToManyField(Tag)
+	comments = models.ManyToManyField(Comment)
 
 	def __str__(self):
 		return "Tytul: " + self.title + ", Tresc: " + self.content
